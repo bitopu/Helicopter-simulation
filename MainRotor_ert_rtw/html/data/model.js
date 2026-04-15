19d92346dc2c269fcd04b28fb0f242603d6c96e4
binhtpt <tongphuocthanbinh@gmail.com>
2026-04-15 11:01:27 +0700

    S

--- MainRotor_ert_rtw/html/data/model.js
+++
@@ -1,1 +0,0 @@
-var modelInfo = {model:"MainRotor"};var modelHierarchy=[{model:"MainRotor",relativePath:"_internal.html",parent:"null"},];var AddCode = true;var ProtectedModel = false;
--- TailRotor_ert_rtw/html/data/model.js
+++
@@ -1,1 +0,0 @@
-var modelInfo = {model:"TailRotor"};var modelHierarchy=[{model:"TailRotor",relativePath:"_internal.html",parent:"null"},];var AddCode = true;var ProtectedModel = false;
